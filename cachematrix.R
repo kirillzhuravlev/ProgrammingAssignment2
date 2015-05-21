45fc2ad67f00d9348c30f984409797a2b7532a96
kirillzhuravlev <kirillzhuravlev@gmail.com>
2015-05-21 18:36:21 -0500

    assignment file

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,7 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This code computes the inverse of a matrix. If the inversion of a matrix 
+## is called, the function looks in cache and if the inverse is already there, 
+## it takes the result from the cache. If the cache is empty, the function calculates the inverse
+## of the given matrix and saves its value in cache for future use.
 
 ## Write a short comment describing this function
 
@@ -10,18 +12,18 @@ makeCacheMatrix <- function(A = matrix()) {#caches the inverse of a matrix
     Inv <<- NULL
   }
   get <- function() A #gets the matrix
-  setinverse <- function(inv) Inv <<- inv  #caches the inverse of the matrix determined by the cacheSolve function
+  setinverse <- function(inv) Inv <<- inv  #caches the inverse of the matrix
+                                            # determined by the cacheSolve function
   getinverse <- function() Inv #gets the inverse from cache
-  list(set = set, get = get,
-       setinverse = setinverse,
-       getinverse = getinverse)
+      list(set = set, get = get,
+        setinverse = setinverse,
+          getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) {## Return a matrix that is the inverse of 'x'
 
  I <- x$getinverse() #gets the inverse from cache
   
@@ -30,9 +32,10 @@ cacheSolve <- function(x, ...) {
     return(I) #returns the value stored in cache
   }
   data1 <- x$get() #gets the matrix to be inverted
- print(data1) #prints the matrix
+    print(data1) #prints the matrix
+ 
   I <- solve(data1) #inverts the matrix
-  x$setinverse(I) #sets the inverse, which will be stored in cache
-  I #returns the inverse matrix
+    x$setinverse(I) #sets the inverse, which will be stored in cache
+      I #returns the inverse matrix
 
 }
